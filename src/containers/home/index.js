f87ea213da2e57773eca57b7267d4acc85c0f941
Nicolas Wagner <contact@wagner-nicolas.com>
2018-01-22 08:48:08 +0100

    feat: add metro style
    from https://github.com/n1c01a5/kleros-contractors-front

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,23 +1,136 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import { Provider, connect } from 'react-redux'
+// import _ from 'lodash'
+import Blockies from 'react-blockies'
+// import * as contractsDux from '../duxs/contracts'
 
-import Balance from '../balance'
+import './home.css'
 
-const Home = ({ str }) => (
-  <div>
-    <b>Hello CryptoWorld{str}</b>
-    <br />
-    <br />
-    <Balance />
-  </div>
-)
+class App extends Component {
+  state = {
+    addingContract: false,
+    loadingContracts: false,
+    creatingContract: false
+  }
 
-Home.propTypes = {
-  str: PropTypes.string
+  componentDidMount() {
+    // const { fetchContracts } = this.props
+    // fetchContracts()
+  }
+
+  render() {
+    return (
+      <Provider store={this.props.store}>
+        <div>
+          <div className="container">
+            <div className="flex-container">
+              <div className="flex-item wide contract grow">
+                <div className="type">Profile</div>
+                <Blockies seed="Jeremy" size={10} scale={14} bgColor="#fff" />
+                <div className="content">
+                  <div className="address">{'0x4d010...87f'}</div>
+                  <div className="balance">0 PNK</div>
+                  <div className="activate_pnk">Activate</div>
+                </div>
+              </div>
+
+              <div className="flex-item wide grow newContract">
+                <div>New Contract</div>
+              </div>
+
+              <div className="flex-item wide apps grow">
+                <Blockies seed="ewfwef" size={10} scale={14} bgColor="#fff" />
+              </div>
+
+              <div className="flex-item wide media grow">
+                <Blockies
+                  seed="Jere[pdemy"
+                  size={10}
+                  scale={14}
+                  bgColor="#fff"
+                />
+              </div>
+
+              <div className="flex-item wide media">
+                <Blockies
+                  seed="Jewderemy"
+                  size={10}
+                  scale={14}
+                  bgColor="#fff"
+                />
+              </div>
+
+              <div className="flex-item wide utility">
+                <Blockies
+                  seed="Jerwdwdemy"
+                  size={10}
+                  scale={14}
+                  bgColor="#fff"
+                />
+              </div>
+
+              <div className="flex-item wide media">
+                <Blockies
+                  seed="Jere[pdemy"
+                  size={10}
+                  scale={14}
+                  bgColor="#fff"
+                />
+              </div>
+
+              <div className="flex-item wide calendar">
+                <Blockies
+                  seed="Jere[pdemy"
+                  size={10}
+                  scale={14}
+                  bgColor="#fff"
+                />
+              </div>
+
+              <div className="flex-item wide games">
+                <Blockies
+                  seed="Jere[pdemy"
+                  size={10}
+                  scale={14}
+                  bgColor="#fff"
+                />
+              </div>
+
+              <div className="flex-item wide utility">
+                <Blockies
+                  seed="Jere[pdemy"
+                  size={10}
+                  scale={14}
+                  bgColor="#fff"
+                />
+              </div>
+
+              <div className="flex-item wide fun">
+                <Blockies
+                  seed="Jere[pdemy"
+                  size={10}
+                  scale={14}
+                  bgColor="#fffa7a" // timeout contract
+                />
+              </div>
+
+              <div className="flex-item wide apps">
+                <Blockies
+                  seed="Jere[pdemy"
+                  size={10}
+                  scale={14}
+                  bgColor="#fff"
+                />
+              </div>
+            </div>
+            <div className="footer">Footer</div>
+          </div>
+        </div>
+      </Provider>
+    )
+  }
 }
 
-Home.defaultProps = {
-  str: '!'
-}
-
-export default Home
+export default connect(state => ({}), {
+  // fetchContracts: contractsDux.fetchContracts,
+})(App)
